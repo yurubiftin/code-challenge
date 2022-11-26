73f6f5cfd8e3d081ba1cecb4ddc348bfb58a080f
yurubahmed <yurubiftin2002@gmail.com>
2022-11-26 18:23:15 -0500

    completed assignment

---
+++ Javascript/challenge-2.js
@@ -0,0 +1,10 @@
+const prompt= require("prompt-sync")();
+var speed = prompt("Enter your speed")
+if(speed<70){
+    console.log("Ok")}
+else
+{
+console.log("Points:"+((speed-70)/5));
+if((speed-70)/5>12)
+console.log(LicenseSuspended);
+}
