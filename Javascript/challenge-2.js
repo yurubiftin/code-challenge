75ced4f8c3956da179e561bd4b1b83410074244e
yurubahmed <yurubiftin2002@gmail.com>
2022-11-26 19:05:38 -0500

    completed assignment

--- Javascript/challenge-2.js
+++ Javascript/challenge-2.js
@@ -1,10 +1,12 @@
 const prompt= require("prompt-sync")();
 var speed = prompt("Enter your speed")
+//if the speed is less than 70 , it should print "Ok"
 if(speed<70){
-    console.log("Ok")}
+    console.log("Ok")} 
 else
 {
 console.log("Points:"+((speed-70)/5));
+//if the driver gets more than 12 points, the function should print: "Licence suspended".
 if((speed-70)/5>12)
 console.log(LicenseSuspended);
 }
